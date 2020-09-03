94fdfd165ecdb895122f9f292ab0ff534a950518
woooyoung <axdsw121@gmail.com>
2020-09-03 16:58:23 +0900

    20200903 Share

---
+++ Share/햄버거버튼/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    var burger = $('.menu-trigger');
+
+    burger.each(function(index){
+        var $this = $(this);
+        
+        $this.on('click', function(e){
+            e.preventDefault();
+            $(this).toggleClass('active-' + (index+1));
+        })
+    });
+
+});
